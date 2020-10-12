718bf607226ac7b0c38d57b9c90cd1f33a9dd7c5
David Wampamba <davidwampamba@gmail.com>
2020-10-12 22:58:09 +0300

    Login successful

--- models/user.js
+++ models/user.js
@@ -79,21 +79,16 @@ userSchema.statics.findByCredentials = async function( phone = '', email = '', u
 
     if( email != '' ) {
         user = await User.findOne({email: email})
-        console.log('Email: ', email)
 
     } else if( phone != '' ) {
         user = await User.findOne({phone: phone})
-        console.log('Phone: ', phone)
 
     } else if( username != '' ) {
         user = await User.findOne({username: username})
-        console.log( 'Username: ', username)
 
     }
 
     if ( !user || user == null || user.length == 0 ) throw new Error( `User not found ${phone}, ${email}, ${username}` )
-
-    // console.log(user)
 
     const isPasswordMatch = await bcrypt.compare( password, user.password )
 
