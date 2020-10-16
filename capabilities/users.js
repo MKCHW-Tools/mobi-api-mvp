8c687874f63e40d2bf6c3f406243f4d272a3334f
David Wampamba <davidwampamba@gmail.com>
2020-10-16 22:44:41 +0300

    View profile

--- capabilities/users.js
+++ capabilities/users.js
@@ -1,16 +1,15 @@
 const {ROLES} = require('../roles')
 
 const canUpdateUser = (editor, userId) => {
-    editorID = parseInt(editor._id)
-    userID = parseInt(userId)
+    editorID = String(editor._id)
+    userID = String(userId)
     return editor.roles.includes(ROLES.ADMIN) || editorID === userID
 }
 
 const canViewProfile = (viewer, profileId) => {
     viewerID = String(viewer._id)
     profileID = String(profileId)
-    console.log( 'viewer ', viewerID , 'user',  profileID)
-    return viewer.roles.includes(ROLES.ADMIN) /*|| viewerID === profileID*/
+    return viewer.roles.includes(ROLES.ADMIN) || viewerID === profileID
 }
 
 module.exports = {
