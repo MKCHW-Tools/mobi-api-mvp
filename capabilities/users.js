4ac28f6870ea1928eba502fc9f17bdbf13d9fd69
David Wampamba <davidwampamba@gmail.com>
2020-10-16 14:21:18 +0300

    User can edit themselves

--- capabilities/users.js
+++ capabilities/users.js
@@ -3,8 +3,8 @@ const {ROLES} = require('../roles')
 const canUpdateUser = (editor, userId) => {
     console.log('Editor', editor._id)
     console.log('UserId', userId)
-    
-    return editor.roles.includes(ROLES.ADMIN) || editor._id === userId
+
+    return editor.roles.includes(ROLES.ADMIN) || editor._id == userId
 }
 
 const canViewUserProfile = (user, profileId) => {
