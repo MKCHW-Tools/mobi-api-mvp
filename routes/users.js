4ec81564e2178c6bc3da34447bf8bbaffaeaead2
David Wampamba <davidwampamba@gmail.com>
2020-10-16 13:56:04 +0300

    User can edit themselves

--- routes/users.js
+++ routes/users.js
@@ -99,7 +99,7 @@ const authUpdateUser = async (req, res, next) => {
     const editor = await User.getUser(req.body.editor)
 
     if(!editor) return res.status(403).send('Not Allowed')
-
+    console.log(req.user._id)
     if(!canUpdateUser(editor, req.user._id)) {
         return res.status(403).send('Not Allowed')
     }
