eb309a760fcee75658994ff45bbec5cbff496545
David Wampamba <davidwampamba@gmail.com>
2020-10-17 13:57:57 +0300

    Pagination, set defaults start 0 and limit 10

--- routes/users.js
+++ routes/users.js
@@ -136,7 +136,7 @@ router.get('/users/:id', auth, authProfileViewer, async (req, res) => {
 })
 
 router.get('/users', auth, authRole(ROLES.ADMIN), async (req, res) => {
-    const page = parseInt(req.query.page) || 0
+    const page = parseInt(req.query.page) || 1
     const limit = parseInt(req.query.limit) || 10
 
     const startIndex = ( page - 1 ) * limit
