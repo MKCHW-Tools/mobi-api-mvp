64789f6fd488709100329f623546b226a3854a7f
David Wampamba <davidwampamba@gmail.com>
2020-10-17 16:16:56 +0300

    Stop app from breaking because of expired jwt token

--- helpers/auth.js
+++ helpers/auth.js
@@ -8,18 +8,29 @@ const auth = async (req, res, next) => {
         return res.status(403).send('Missing token')
 
     const token = req.header('Authorization') && req.header('Authorization').replace('Bearer ', '')
-    if( token == null || token == '' || token.length == 0) return res.status(403).send('You need to sign in')
+    if( !token ) return res.status(403).send('You need to sign in')
+    try {
+        const jwtInfo = jwt.verify(token, process.env.JWT_KEY)
 
-    const jwtInfo = jwt.verify(token, process.env.JWT_KEY)
+        const user = await User.findOne({_id: jwtInfo._id, 'tokens.token': token})
 
-    const user = await User.findOne({_id: jwtInfo._id, 'tokens.token': token})
+        if (!user) return res.status(403).send('You need to sign in')
+        
+        req.user = user
+        // req.token = token
 
-    if (!user) return res.status(403).send('You need to sign in')
-    
-    req.user = user
-    // req.token = token
+        next()
+    } catch( e ) {
+        
+        if(e instanceof TokenExpiredError) {
+            console.log(e.message)
+            res.status(403).json({
+                result: "Failure",
+                msg: 'Expired token'
+            })
+        }
 
-    next()
+    }
 }
 
 const authRole = role => {
