35bcb8e44fd383673ab189251339addace3c87e6
David Wampamba <davidwampamba@gmail.com>
2020-10-17 17:55:57 +0300

    Generate password on user update

--- models/user.js
+++ models/user.js
@@ -69,12 +69,10 @@ const userSchema = mongoose.Schema({
 })
 
 userSchema.methods.generateAuthToken = async function() {
-
     const user = this
     const token = jwt.sign({_id: user._id }, process.env.KEY, { expiresIn: '1800s' })
     user.tokens = user.tokens.concat({ token })
     await user.save()
-
     return token
 }
 
