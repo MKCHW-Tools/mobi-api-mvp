ffa4bb7340ec70022b90f7eaa78292e9a49790ac
David Wampamba <davidwampamba@gmail.com>
2020-10-19 13:15:31 +0300

    Turn signRefreshToken to async

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken')
 
 const signRefreshToken = async userid => {
     const savedUser = await User.findOne({_id: userid})
+    console.log(savedUser)
     const refreshToken = jwt.sign({}, {
         userId: savedUser._id
     }, process.env.REFRESH_KEY, {expiresIn: '1m'})
