7cc456c105d1a3c738bb0bc84550c9dad2a5657b
David Wampamba <davidwampamba@gmail.com>
2020-10-19 13:04:51 +0300

    JWT path fixed

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 const signRefreshToken = userid => {
-
     const savedUser = await User.findOne({_id: userid})
+    console.log(savedUser)
+
     const refreshToken = jwt.sign({}, {
         userId: savedUser._id
     }, process.env.REFRESH_KEY, {expiresIn: '1m'})
