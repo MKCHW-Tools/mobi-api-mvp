2f6acc191614d79126dddbd3dccb6c59464e28c1
David Wampamba <davidwampamba@gmail.com>
2020-10-19 12:56:51 +0300

    Fix ACCESS_KEY

--- models/user.js
+++ models/user.js
@@ -74,7 +74,7 @@ const userSchema = mongoose.Schema({
 
 userSchema.methods.generateAuthToken = async function() {
     const user = this
-    const token = jwt.sign({_id: user._id }, process.env.KEY, {expiresIn: '1800s'})
+    const token = jwt.sign({_id: user._id }, process.env.ACCESS_KEY, {expiresIn: '1800s'})
     user.tokens = user.tokens.concat({ token })
 
     await user.save()
