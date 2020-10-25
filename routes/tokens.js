d13329bf51b5bf2d70ba172b4759e42697331a83
David Wampamba <davidwampamba@gmail.com>
2020-10-26 01:34:59 +0300

    Bugfix for token invalidation

--- routes/tokens.js
+++ routes/tokens.js
@@ -66,9 +66,9 @@ router.post('/tokens/in-validate-token', auth, authInvalidateTokens, async (req,
     })
 
     tokens.forEach(token => {
-        const owner = (type == 1) ? User.findOne({accesToken: token.accessToken}) : User.findOne({refreshToken: token.refreshToken})
+        const owner = (token.type == 1) ? User.findOne({accesToken: token.accessToken}) : User.findOne({refreshToken: token.refreshToken})
         if(owner._id) {
-            (type == 1) ? User.update(owner._id,{accessToken:''}) : User.update(owner._id,{refreshToken:''})
+            (token.type == 1) ? User.update(owner._id,{accessToken:''}) : User.update(owner._id,{refreshToken:''})
         }
     })
 
