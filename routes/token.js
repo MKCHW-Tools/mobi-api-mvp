0dc9aef223b12549821d31cd8d316f74c6a7c4ce
David Wampamba <davidwampamba@gmail.com>
2020-10-25 08:40:21 +0300

    Bug fix validateToken is not a function

--- routes/token.js
+++ routes/token.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const validateToken = require('../helpers/validate.token')
+const {validateToken} = require('../helpers/validate.token')
 const User = require('../models/user')
 
 const router = express.Router()
