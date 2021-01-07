506648754574ce525b5da7db08b0843e64d305a5
David Wampamba <davidwampamba@gmail.com>
2021-01-07 14:44:36 +0300

    Some refactoring

--- routes/doctors.js
+++ routes/doctors.js
@@ -2,7 +2,6 @@ const express = require('express')
 const {doctors, doctor} = require('../controllers/doctors')
 const User = require('../models/user')
 const {auth} = require('../helpers/authorize')
-const {canViewProfile} = require('../capabilities/users')
 const {paginate} = require('../helpers/pagination')
 
 const router = express.Router()
