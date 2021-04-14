be122390180228bf76b6bcc46595f35ff3663677
David Wampamba <davidwampamba@gmail.com>
2021-04-14 15:55:45 +0300

    Debug doctors

--- routes/doctors.js
+++ routes/doctors.js
@@ -1,12 +1,13 @@
 const express = require('express')
-const {doctors, doctor} = require('../controllers/doctors')
+// const {doctors, doctor} = require('../controllers/doctors')
 const User = require('../models/user')
 const {auth} = require('../helpers/authorize')
 const {paginate} = require('../helpers/pagination')
 
 const router = express.Router()
 
-router.get('/doctors/:id', auth, doctor)
-router.get('/doctors', auth, paginate(User), doctors)
+// router.get('/doctors/:id', auth, doctor)
+// router.get('/doctors', auth, paginate(User), doctors)
+router.get('/doctors', auth, paginate(User), ['David','John','Joan'])
 
 module.exports = router
