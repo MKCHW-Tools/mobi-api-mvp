6f1a932e2c653149edb6fae2daff1e293d763b4d
David Wampamba <davidwampamba@gmail.com>
2021-04-26 15:28:18 +0300

    Introduce users/exams routes

--- routes/users.js
+++ routes/users.js
@@ -251,7 +251,7 @@ userRouter.put('/users/:id', auth, authUpdateUser, async (req, res) => {
     
 })
 
-userRouter.delete('/users/delete/:id', auth, authRole(ROLES.ADMIN), async (req, res) => {
+userRouter.delete('/delete/:id', auth, authRole(ROLES.ADMIN), async (req, res) => {
     
     const id = req.params.id
     
