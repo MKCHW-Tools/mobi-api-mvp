d7ad39227e62c5078e2c35813ba57cf6125c2b21
Matt-Murungi <murungima@gmail.com>
2022-09-01 21:41:56 +0300

    created chat and message models

--- db/db.js
+++ db/db.js
@@ -1,10 +1,16 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    serverSelectionTimeoutMS: 5000
-}).catch( error => console.error(error))
+mongoose
+  .connect(
+    process.env.MONGODB_URL,
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      serverSelectionTimeoutMS: 5000,
+    },
+    console.log("connected db")
+  )
+  .catch((error) => console.error(error));
--- models/chat.js
+++ models/chat.js
@@ -1,71 +1,17 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
-const validator = require('validator')
-const threadSignature = {
-    msg:{
-        type: String,
-        required: true
+const mongoose = require("mongoose");
+
+const chatModel = mongoose.Schema(
+  {
+    chatName: { type: String, trim: true },
+    users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
     },
-    sender:{
-        name:{
-            type: String,
-            required: true
-        },
-        id: {
-            type: String,
-            required: true
-        }
-    }
-}
-const chatSignature = {
-        to: {
-            name:{
-                type: String,
-                required: true
-            },
-            id: {
-                type: String,
-                required: true
-            }
-        },
-        startedBy: {
-            name:{
-                type: String,
-                required: true
-            },
-            id: {
-                type: String,
-                required: true
-            }
-        }
-}
+  },
+  { timestamps: true }
+);
 
-const threadSchema = new mongoose.Schema(threadSignature,{timestamps: true});
-chatSignature['thread'] = [threadSchema]
-const chatSchema = mongoose.Schema(chatSignature, {timestamp: true})
+const Chat = mongoose.model("Chat", chatModel);
 
-chatSchema.statics.getChats = async () => {
-    const chats = await Chat.find({})
-    return chats
-}
-
-chatSchema.statics.getChat = async id => {
-    const chat = await Chat.findOne({_id: id})
-    return chat
-}
-
-chatSchema.statics.update = async (id, data) => {
-
-    const chat = await Chat.findByIdAndUpdate( id, data, {new: true})
-    return chat
-}
-
-chatSchema.statics.delete = async id => {
-    const chat = await Chat.findByIdAndRemove(id)
-    return chat
-
-}
-
-const Chat = mongoose.model('Chat', chatSchema)
-
-module.exports = Chat
+module.exports = Chat;
